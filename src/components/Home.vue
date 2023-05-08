2bd1a7bfc74860248cc5aab263ecaf83387fae73
cerensolpan <cerensolpan@gmail.com>
2023-05-08 18:43:38 +0300

    feat: Create tree component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,39 @@
 <template>
-  <div>Home Component</div>
+  <div class="container">
+    <div class="container__animation">
+      <Tree />
+    </div>
+  </div>
 </template>
 
 <script>
+import Tree from "./Tree/index.vue";
+
 export default {
   name: "HomeComponent",
+  components: {
+    Tree,
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.container {
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: #1d976c;
+  background: -webkit-linear-gradient(to bottom, #99b290, #569340);
+  background: linear-gradient(to bottom, #c1edb1, #569340);
+}
+
+.container__animation {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-bottom: 60px;
+}
+</style>
---
+++ src/components/Tree/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="tree" @click="shakeTree">
+    <img
+      :src="require('@/assets/treeImg.svg')"
+      :class="{ shaking: isShaking }"
+      id="tree"
+    />
+  </div>
+</template>
+
+<script>
+import "./style.css";
+import { computed } from "vue";
+import store from "../../store";
+
+export default {
+  name: "treeComponent",
+
+  setup() {
+    function shakeTree() {
+      store.dispatch("shakeTree");
+    }
+
+    return {
+      isShaking: computed(() => store.state.isShaking),
+      shakeTree,
+    };
+  },
+};
+</script>
