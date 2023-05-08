4b5d2568d68bcd336b944224ac6334e2f6b985ce
cerensolpan <cerensolpan@gmail.com>
2023-05-08 19:46:24 +0300

    feat: Create button component

---
+++ src/components/UI/Button/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="button">
+    <button class="button__btn" type="button">
+      {{ text }}
+    </button>
+  </div>
+</template>
+
+<script>
+import "./style.css";
+
+export default {
+  name: "ButtonComponent",
+  props: {
+    text: String,
+  },
+};
+</script>
